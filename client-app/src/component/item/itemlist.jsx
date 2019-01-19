ba87c2f867490f4bd3962433ab3bebaa5eca27b2
Peter <peter.luangrath@slalom.com>
2019-01-18 16:41:35 -0800

    added total to orderlist, clean up code

--- client-app/src/component/item/itemlist.jsx
+++ client-app/src/component/item/itemlist.jsx
@@ -2,16 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-
-
-
-
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-
 
 const styles = theme => ({
   root: {
@@ -32,11 +27,10 @@ const styles = theme => ({
   }
 });
 
-
 const itemPhoto = {
   height: '75px',
   width: '75px'
-};
+};{/*TODO: share styles*/}
 
 class ItemList extends React.Component {
   constructor(props) {
@@ -45,60 +39,47 @@ class ItemList extends React.Component {
       order: [],
       items: []
     };
-
-   // this.addItem = this.addItem.bind(this);
- }
-  
+  }
 
   addItem(item) {
     this.state.order.push(item);
+    this.setState({ order: this.state.order })
     localStorage.setItem("order", JSON.stringify(this.state.order));
-    //this.forceUpdate();
   }
 
   render() {
-  
-    const { items } = this.state;
-
     return (
-
       <div>
         <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Id</TableCell>
-            <TableCell align="right"></TableCell>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Price</TableCell>
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {items.map(item => (
-            <TableRow key={item.id}>
-              <TableCell component="th" scope="row">
-                Going to be ID
-              </TableCell>
-              <TableCell align="right"><img style={itemPhoto} src={require(`../../images/${item.imageFileName}`)} alt="burger"></img></TableCell>
-              <TableCell align="right">{item.name}</TableCell>
-              <TableCell align="right">{item.price}</TableCell>
-              <TableCell align="right"><Button onClick={this.addItem.bind(this, item)}>Add</Button></TableCell>
+          <TableHead>
+            <TableRow>
+              <TableCell align="right"></TableCell> {/*image column*/}
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right"></TableCell> {/*add button column*/}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-
-      <div>
-              {this.state.order.map(item =>
-                <div>{item.name}</div>
-                )}
+          </TableHead>
+          <TableBody>
+            {this.state.items.map(item => (
+              <TableRow>
+                <TableCell align="right"><img style={itemPhoto} src={require(`../../images/${item.imageFileName}`)} alt="burger"></img></TableCell>
+                <TableCell align="right">{item.name}</TableCell>
+                <TableCell align="right">{item.price}</TableCell>
+                <TableCell align="right"><Button onClick={this.addItem.bind(this, item)}>Add</Button></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <div>
+          <span><b>Items added thus far: </b></span>
+          {this.state.order.map(item =>
+            <span>{item.name} {item.price}, </span>
+          )}
+          {/*TODO: should persist in state for refresh*/}
+        </div>
       </div>
-    </div>
-
-   
     );
   }
-  //() => this.handleSort(column)}
 
   componentDidMount() {
     fetch("http://localhost:8080/api/v1/items")
@@ -112,78 +93,17 @@ class ItemList extends React.Component {
           items: result
         });
       },
-      // Note: it's important to handle errors here
-      // instead of a catch() block so that we don't swallow
-      // exceptions from actual bugs in components.
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-    )
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 }
 
-ItemList.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
 export default withStyles(styles)(ItemList);
-
-
-/*
-
-              <TextField
-                  id="imageFileName"
-                  label="imageFileName"
-                  multiline
-                  rows="4"
-                  name={item.imageFileName}
-                  value={item.imageFileName}
-                  defaultValue={item.imageFileName}
-                  onChange={this.onChange}
-                  className={classes.textField}
-                  margin="normal"
-                />
-                <TextField
-                  id="ItemId"
-                  label="Item Id"
-                  multiline
-                  rows="4"
-                  name={item.id + "--id"}
-                  value={item.id}
-                  defaultValue={item.id}
-                  onChange={this.onChange}
-                  className={classes.textField}
-                  margin="normal"
-                />
-                <TextField
-                  id="ItemName"
-                  label="Item name"
-                  multiline
-                  name={item.id + "--name"}
-                  value={item.name}
-                  rows="4"
-                  defaultValue={item.name}
-                  onChange={this.onChange}
-                  className={classes.textField}
-                  margin="normal"
-                />
-                <TextField
-                  id="ItemPrice"
-                  label="Item price"
-                  multiline
-                  rows="4"
-                  name={item.id + "--price"}
-                  value={item.price}
-                  defaultValue={item.price}
-                  onChange={this.onChange}
-                  className={classes.textField}
-                  margin="normal"
-                />
-                <Button variant="outlined" color="primary" className={classes.button} type="submit">
-                  Add
-                </Button>
-
-*/
--- client-app/src/component/order/orderlist.jsx
+++ client-app/src/component/order/orderlist.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -8,56 +7,51 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
 const itemPhoto = {
-    height: '75px',
-    width: '75px'
-  };
-
+  height: '75px',
+  width: '75px'
+};
 
 class OrderList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      order: JSON.parse(localStorage.getItem("order")),
+      total: 0
+    };
+  }
+  removeItem(item) {
+    //TODO: Implement
+  }
 
-
-
-
-
-    constructor(props) {
-       
-        super(props);
-        this.state = {
-            order : JSON.parse( localStorage.getItem("order"))
-        };
-    }
-    removeItem(item) {
-            //TODO: Implement
-    }
-    render() {
-        return (
-
+  render() {
+    return (
+      <div>
         <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Id</TableCell>
-            <TableCell align="right"></TableCell>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Price</TableCell>
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {this.state.order.map(item => (
-            <TableRow key={item.id}>
-              <TableCell component="th" scope="row">
-                Going to be ID
-              </TableCell>
-              <TableCell align="right"><img style={itemPhoto} src={require(`../../images/${item.imageFileName}`)} alt="burger"></img></TableCell>
-              <TableCell align="right">{item.name}</TableCell>
-              <TableCell align="right">{item.price}</TableCell>
-              <TableCell align="right"><Button onClick={this.removeItem.bind(this, item)}>Remove</Button></TableCell>
+          <TableHead>
+            <TableRow>
+            <TableCell align="right"></TableCell> {/*image column*/}
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right"></TableCell> {/*remove button column*/}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-        )
-    }
+          </TableHead>
+          <TableBody>
+            {this.state.order.map(item => (
+              this.state.total += item.price,
+              <TableRow>
+                <TableCell align="right"><img style={itemPhoto} src={require(`../../images/${item.imageFileName}`)} alt="burger"></img></TableCell>
+                <TableCell align="right">{item.name}</TableCell>
+                <TableCell align="right">{item.price}</TableCell>
+                <TableCell align="right"><Button onClick={this.removeItem.bind(this, item)}>Remove</Button></TableCell>
+              </TableRow>
+            ))
+            }
+          </TableBody>
+        </Table>
+        <span>Total: {this.state.total}</span>
+      </div>
+    )
+  }
 }
 
 export default (OrderList);
